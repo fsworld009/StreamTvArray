04b85bbb05327bf85a3fb082f8f5a69549d577d2
DannyAAM <danny@saru.moe>
2021-04-04 03:27:17 +0800

    fix for new requirements for twitch embedding

--- source/components/StreamTwitch.vue
+++ source/components/StreamTwitch.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="stream-cell">
-    <iframe class="stream" :src="'https://player.twitch.tv/?channel='+options.channel" frameborder="0" allowfullscreen="true" scrolling="no" @load="onStreamLoad"></iframe>
+    <iframe class="stream" :src="'https://embed.twitch.tv/?layout=video&parent='+host+'&channel='+options.channel" frameborder="0" allowfullscreen="true" scrolling="no" @load="onStreamLoad"></iframe>
     <div class="chat-container" :style="[chatStyle, {width: chatHover?'50%':chatStyle.width} ]" v-if="options.openChat" @mouseenter="onHoverChat(true)" @mouseleave="onHoverChat(false)">
-      <iframe :src="'https://www.twitch.tv/embed/'+options.channel+'/chat?darkpopout=true'" frameborder="0" scrolling="no"></iframe>
+      <iframe :src="'https://www.twitch.tv/embed/'+options.channel+'/chat?parent='+host" frameborder="0" scrolling="no"></iframe>
     </div>
   </div>
 </template>
@@ -17,6 +17,7 @@ export default {
   },
   data(){
     return {
+      host: location.host,
       chatHover: false
     }
   },
